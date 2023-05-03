686b919f4f009cd52d50df5570cd0848a9f83598
Dima-Ant <dimaantifeyev@gmail.com>
2023-05-03 10:51:48 +0300

    JS First

---
+++ HW1/script.js
@@ -0,0 +1,15 @@
+let valuecost = prompt('Вартість товару');
+let discount = prompt('Знижка');
+let resultValue = Number(valuecost) - Number(discount);
+
+console.log('Вартість зі знижкою:', resultValue);
+
+
+let UserName = prompt('Введіть ваше імя');
+console.log('Імя користувача:', UserName);
+
+let sideA = prompt('Введіть значення сторони A');
+let sideB = prompt('Введіть значення сторони B');
+let area = Number(sideA) * Number(sideB);
+
+console.log('Площа прямокутника:', area);
